bc567ce46149509744496c79081dbcb4d8821066
elize408-sketch <elize408@gmail.com>
2025-12-09 09:46:37 +0100

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
-    'Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables!'
+    'Missing EXPO_PUBLIC_SUPABASE_URL or EXPO_PUBLIC_SUPABASE_ANON_KEY environment variables!'
   );
 }
 
