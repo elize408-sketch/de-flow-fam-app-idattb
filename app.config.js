8dd4cab372f2d0952aff9786c5afaecae13472d2
elize408-sketch <elize408@gmail.com>
2025-12-14 11:44:36 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => ({
     usesAppleSignIn: true,
   },
   extra: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_URL: "https://iykrwfgfdpnlfmdexrpr.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5a3J3ZmdmZHBubGZtZGV4cnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNTA4ODIsImV4cCI6MjA3OTYyNjg4Mn0.e2KS_hzDwXb-oGPQW7tC6g70Wo5CDMVb61gGVqPiYTIY",
   },
 });
