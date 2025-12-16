582f0f64846edaf0c3e6ed196f412b8600540514
elize408-sketch <elize408@gmail.com>
2025-12-16 10:02:57 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,81 +1,91 @@
 export default ({ config }) => ({
   ...config,
-  expo: {
-    ...config.expo,
-    name: "Flow Fam",
-    slug: "flow-fam",
-    version: "1.0.6",
-    orientation: "portrait",
-    icon: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
-    userInterfaceStyle: "automatic",
-    splash: {
-      image: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
-      resizeMode: "contain",
-      backgroundColor: "#F9F6F1"
+
+  name: "Flow Fam",
+  slug: "flow-fam",
+  version: "1.0.6",
+  orientation: "portrait",
+
+  icon: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
+  userInterfaceStyle: "automatic",
+
+  splash: {
+    image: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
+    resizeMode: "contain",
+    backgroundColor: "#F9F6F1",
+  },
+
+  scheme: "flowfam",
+
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "nl.flowfam.gezinsapp",
+    deploymentTarget: "16.0",
+    buildNumber: "2",
+    usesAppleSignIn: true,
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+      CFBundleDisplayName: "Flow Fam",
+      CFBundleName: "FlowFam",
     },
-    scheme: "flowfam",
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "nl.flowfam.gezinsapp",
-      deploymentTarget: "16.0",
-      buildNumber: "2",
-      usesAppleSignIn: true,
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false,
-        CFBundleDisplayName: "Flow Fam",
-        CFBundleName: "FlowFam"
-      }
+  },
+
+  android: {
+    package: "nl.flowfam.gezinsapp",
+    versionCode: 2,
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
+      backgroundColor: "#F9F6F1",
     },
-    android: {
-      package: "nl.flowfam.gezinsapp",
-      versionCode: 2,
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
-        backgroundColor: "#F9F6F1"
+    edgeToEdgeEnabled: true,
+  },
+
+  web: {
+    favicon: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
+    bundler: "metro",
+  },
+
+  plugins: [
+    "expo-font",
+    "expo-router",
+    "expo-web-browser",
+    [
+      "@react-native-google-signin/google-signin",
+      {
+        iosUrlScheme:
+          "com.googleusercontent.apps.143b077c-c9bc-49ad-8f27-0180e47a6e1a",
       },
-      edgeToEdgeEnabled: true
-    },
-    web: {
-      favicon: "./assets/images/ad140c28-4e3a-4941-a32b-028b482df723.png",
-      bundler: "metro"
-    },
-    plugins: [
-      "expo-font",
-      "expo-router",
-      "expo-web-browser",
-      [
-        "@react-native-google-signin/google-signin",
-        {
-          iosUrlScheme: "com.googleusercontent.apps.143b077c-c9bc-49ad-8f27-0180e47a6e1a"
-        }
-      ],
-      [
-        "expo-build-properties",
-        {
-          ios: {
-            deploymentTarget: "16.0",
-            useFrameworks: "static",
-            flipper: false
-          },
-          android: {
-            compileSdkVersion: 34,
-            targetSdkVersion: 34,
-            minSdkVersion: 23
-          }
-        }
-      ]
     ],
-    experiments: {
-      typedRoutes: true,
-      newArchEnabled: true
-    },
-    extra: {
-      router: {},
-      eas: {
-        projectId: "143b077c-c9bc-49ad-8f27-0180e47a6e1a"
+    [
+      "expo-build-properties",
+      {
+        ios: {
+          deploymentTarget: "16.0",
+          useFrameworks: "static",
+          flipper: false,
+        },
+        android: {
+          compileSdkVersion: 34,
+          targetSdkVersion: 34,
+          minSdkVersion: 23,
+        },
       },
-      SUPABASE_URL: "https://iykrwfgfdpnlfmdexrpr.supabase.co",
-      SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5a3J3ZmdmZHBubGZtZGV4cnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNTA4ODIsImV4cCI6MjA3OTYyNjg4Mn0.e2KS_hzDwXb-oGPQW7tC6g70Wo5CDMVb61gGVqPiYTI"
-    }
-  }
+    ],
+  ],
+
+  experiments: {
+    typedRoutes: true,
+    newArchEnabled: true,
+  },
+
+  extra: {
+    ...(config.extra ?? {}),
+    router: {},
+    eas: {
+      projectId: "143b077c-c9bc-49ad-8f27-0180e47a6e1a",
+    },
+    SUPABASE_URL: "https://iykrwfgfdpnlfmdexrpr.supabase.co",
+    SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5a3J3ZmdmZHBubGZtZGV4cnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNTA4ODIsImV4cCI6MjA3OTYyNjg4Mn0.e2KS_hzDwXb-oGPQW7tC6g70Wo5CDMVb61gGVqPiYTI",
+  },
 });
